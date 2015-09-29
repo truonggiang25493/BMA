4fb0e0a58fb0049880878362a0c4835a93cc0bcd
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-29 23:58:29 +0700

    Create BMA.Data
    Repository
    Unit of Work
    Factory

---
+++ BMA.Data/Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMA.Data.Infrastructure
+{
+    public interface IUnitOfWork
+    {
+        bool Commit();
+        void BeginTransaction();
+        bool CommitTransaction();
+    }
+}
