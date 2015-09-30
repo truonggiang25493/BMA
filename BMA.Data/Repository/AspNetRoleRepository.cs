64220375c6d468bc8898b2d15d6dedece2b63273
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-30 16:33:18 +0700

    Add AspNetRoleRepository.cs

---
+++ BMA.Data/Repository/AspNetRoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMA.Data.Infrastructure;
+
+namespace BMA.Data.Repository
+{
+    public class AspNetRoleRepository:RepositoryBase<AspNetRole>,IAspNetRoleRepository
+    {
+        public AspNetRoleRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+        }
+    }
+
+    public interface IAspNetRoleRepository : IRepository<AspNetRole>
+    {
+        
+    }
+}
