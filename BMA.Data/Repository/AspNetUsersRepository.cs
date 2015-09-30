0a443ee7e051e122f992a16c205f7dbe8e8b51af
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-30 14:38:53 +0700

    Update BMA.Data
    Complete Infrastructure

---
+++ BMA.Data/Repository/AspNetUsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using BMA.Data.Infrastructure;
+
+namespace BMA.Data.Repository
+{
+    public class AspNetUserRepository : RepositoryBase<AspNetUser>, IAspNetUserRepository
+    {
+        public AspNetUserRepository(IDatabaseFactory databaseFactory)
+            : base(databaseFactory)
+        {
+        }
+    }
+
+    public interface IAspNetUserRepository : IRepository<AspNetUser>
+    {
+
+    }
+}
