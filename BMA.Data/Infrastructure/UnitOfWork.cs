39f3eecdcda235066d7e0ca97ab352cd1562d408
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-30 15:21:06 +0700

    Add AspNetUserRepository
    Add AspNetUserRepository

---
+++ BMA.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace BMA.Data.Infrastructure
+{
+    public class UnitOfWork
+    {
+        private readonly IDatabaseFactory databaseFactory;
+        private BMAEntities dataContext;
+        private DbContextTransaction contextTransaction;
+
+        /// <summary>
+        /// Constructor create unit of word instance with database factory instance
+        /// </summary>
+        /// <param name="databaseFactory">Database factory instance</param>
+        public UnitOfWork(IDatabaseFactory databaseFactory)
+        {
+            this.databaseFactory = databaseFactory;
+        }
+
+        /// <summary>
+        /// Create new transaction to explance unit of work
+        /// </summary>
+        public void BeginTransaction()
+        {
+            contextTransaction = dataContext.Database.BeginTransaction();
+        }
+
+        protected BMAEntities DataContext
+        {
+            get { return dataContext ?? (dataContext = databaseFactory.Get()); }
+        }
+
+        public bool Commit()
+        {
+            return DataContext.SaveChanges() > 0;
+        }
+
+        /// <summary>
+        /// Try to execute all changings of database which create in this unit of work
+        /// </summary>
+        /// <returns>true if execute successfull</returns>
+        public bool CommitTransaction()
+        {
+            try
+            {
+                var rs = DataContext.SaveChanges();
+                contextTransaction.Commit();
+                return rs > 0;
+            }
+            catch (Exception)
+            {
+                contextTransaction.Rollback();
+                return false;
+            }
+        }
+    }
+}
