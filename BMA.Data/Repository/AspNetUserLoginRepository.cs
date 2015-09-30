6a9d2f1417a21bb4c269e900f283bce128ef98dd
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-30 16:44:31 +0700

    Add AspNetUserLoginRepository

--- BMA.Data/Repository/AspNetUserLoginRepository.cs
+++ BMA.Data/Repository/AspNetUserLoginRepository.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BMA.Data.Infrastructure;
 
 namespace BMA.Data.Repository
 {
-    class AspNetUserLoginRepository
+    public class AspNetUserLoginRepository:RepositoryBase<AspNetUserLogin>,IAspNetUserLoginRepository
     {
-
+        public AspNetUserLoginRepository(IDatabaseFactory databaseFactory) : base(databaseFactory)
+        {
+        }
     }
-    
+
+    public interface IAspNetUserLoginRepository : IRepository<AspNetUserLogin>
+    {
+        
+    }
 }
 
