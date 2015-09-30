054f06f120b196f0ac36027c9cecf9aeb6c9f4eb
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-09-30 16:28:19 +0700

    Add AspNetRoleRepository.cs

--- BMA.Data/BMA.Designer.cs
+++ BMA.Data/BMA.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\FPT\CP Fall 2015\BMA\SourceCode\BMA\BMA.Data\BMA.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\FPT\CP Fall 2015\BMA\BMA\BMA.Data\BMA.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
