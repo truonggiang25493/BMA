4b385723692d4b15aa40835de8169759af8bfc17
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-10-03 14:15:41 +0700

    Login PopupDemo

---
+++ PopupDemo_GiangHHT/PopupDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PopupDemo.Startup))]
+namespace PopupDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
