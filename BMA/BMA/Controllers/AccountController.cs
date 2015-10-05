2bd4d43d7609142cd4f189b5f06d8185d0899731
gaumapxipo <ManhOTSE61045@fpt.edu.vn>
2015-10-05 16:08:39 +0700

    Create project cua anh manh

---
+++ BMA/BMA/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMA.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
---
+++ BMA/BMA/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BMA.Models;
+
+namespace BMA.Controllers
+{
+    public class ProductController : Controller
+    {
+        BMAEntities db = new BMAEntities();
+        //
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cookie()
+        {
+            return View();
+        }
+
+        public ActionResult Saltine()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+	}
+}
