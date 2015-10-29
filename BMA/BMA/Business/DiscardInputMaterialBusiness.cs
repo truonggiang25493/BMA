600f8de2126fa53f5d2626dd6351140ce26ac0c4
vuongnguyen1404 <windy27193@gmail.com>
2015-10-29 11:39:40 +0700

    update input material

---
+++ BMA/BMA/Business/DiscardInputMaterialBusiness.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BMA.Models;
+
+namespace BMA.Business
+{
+    public class DiscardInputMaterialBusiness
+    {
+        private static BMAEntities db = new BMAEntities();
+        public bool AddDiscardInputMaterial(DiscardedInputMaterial discardInputMaterial)
+        {
+            if (discardInputMaterial == null)
+            {
+                return false;
+            }
+            try
+            {
+                db.DiscardedInputMaterials.Add(discardInputMaterial);
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                string s = e.ToString();
+                return false;
+            }
+            return true; 
+            
+        }
+    }
+}
---
+++ BMA/BMA/Controllers/InputBillController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BMA.Models;
+
+namespace BMA.Controllers
+{
+    public class InputBillController : Controller
+    {
+        private BMAEntities db = new BMAEntities();
+        //private InputMaterialBusiness inputMaterialBusiness = new InputMaterialBusiness();
+        //
+        // GET: /InputBill/
+        public ActionResult InputBillIndex()
+        {
+            return View();
+        }
+
+	}
+}
