f6c4faf09b4ff3f7c10230b95a395b6a3b750974
truonggiang25493 <GiangHHTSE60894@fpt.edu.vn>
2015-11-13 13:12:30 +0700

    Add OtherExpense Manage

---
+++ BMA/BMA/Business/OtherExpenseBusiness.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BMA.Models;
+
+namespace BMA.Business
+{
+    public class OtherExpenseBusiness
+    {
+        private BMAEntities db;
+        public OtherExpenseBusiness()
+        {
+            db = new BMAEntities();
+        }
+
+        #region Get list of expense
+
+        public List<OtherExpense> GetOtherExpenseList()
+        {
+            return db.OtherExpenses.OrderBy(m => m.OtherExpenseYearTime).ThenBy(m => m.OtherExpenseMonthTime).ToList();
+        }
+
+
+        #endregion
+    }
+}
---
+++ BMA/BMA/Controllers/OtherExpenseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BMA.Business;
+using BMA.Models;
+
+namespace BMA.Controllers
+{
+    public class OtherExpenseController : Controller
+    {
+        // GET: OtherExpense
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Quản lý chi phí khác";
+            OtherExpenseBusiness business = new OtherExpenseBusiness();
+            List<OtherExpense> resultList = business.GetOtherExpenseList();
+            return View(resultList);
+        }
+    }
+}
