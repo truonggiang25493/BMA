c43ef2f9be02322f11d2d8247b00c55211f86f75
gaumapxipo <ManhOTSE61045@fpt.edu.vn>
2015-12-18 11:13:01 +0700

    hehehe

--- BMA/BMA/Models/Category.cs
+++ BMA/BMA/Models/Category.cs
@@ -14,7 +14,6 @@ namespace BMA.Models
     
     public partial class Category
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Category()
         {
             this.Products = new HashSet<Product>();
@@ -23,7 +22,6 @@ namespace BMA.Models
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
     
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
     }
 }
